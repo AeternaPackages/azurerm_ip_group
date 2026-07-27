1b503a1b7fab97abee53067ea3f18d7cf7fe07eb
Daniel Allen <dan@aeternagroup.net>
2026-07-27 18:29:58 +0100

    Update to schema version v4.81.0

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ locals {
 }
 
 module "ip_groups" {
-  source    = "git::https://github.com/AeternaModules/azurerm_ip_group.git?ref=v4.80.0"
+  source    = "git::https://github.com/AeternaModules/azurerm_ip_group.git?ref=v4.81.0"
   ip_groups = local.ip_groups
 }
 
 module "ip_group_cidrs" {
-  source         = "git::https://github.com/AeternaModules/azurerm_ip_group_cidr.git?ref=v4.80.0"
+  source         = "git::https://github.com/AeternaModules/azurerm_ip_group_cidr.git?ref=v4.81.0"
   ip_group_cidrs = local.ip_group_cidrs
   depends_on     = [module.ip_groups]
 }
